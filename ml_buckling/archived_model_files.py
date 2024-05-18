d75a7fd022dc0d272c25f3f2fdbe148f5fbae642
seanfireball1 <sean.engelstad1@gmail.com>
2024-05-18 17:52:04 -0400

    update the main repo

---
+++ ml_buckling/archived_model_files.py
@@ -0,0 +1,13 @@
+"""
+Paths to archived models
+"""
+__all__ = ["axialGP_csv", "shearGP_csv", "cripplingGP_csv"]
+
+import os
+base_dir = os.path.dirname(os.path.abspath(__file__))
+root_dir = os.path.dirname(base_dir)
+model_dir = os.path.join(root_dir, "archived_models")
+
+axialGP_csv = os.path.join(model_dir, "axialGP.csv")
+shearGP_csv = os.path.join(model_dir, "shearGP.csv")
+cripplingGP_csv = os.path.join(model_dir, "cripplingGP.csv")
