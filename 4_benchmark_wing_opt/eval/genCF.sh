ea76b35f22f0a5d3b6e3100c5bd4e3df47a651e7
Sean Engelstad <sean.engelstad1@gmail.com>
2024-06-17 08:04:38 -0400

    update final deisgns opt 4

--- 4_benchmark_wing_opt/eval/genCF.sh
+++ 4_benchmark_wing_opt/eval/genCF.sh
@@ -2,16 +2,16 @@ rm -rf capsStruct*
 python make_exploded.py --exploded 1
 mv capsStruct1_0/Scratch/tacs/tacs_output_file000.f5 CF-view/uwing.f5
 cd CF-view/
-~/git/tacs/extern/f52vtk/f52vtk_element uwing.f5
+~/git/tacs/extern/f5tovtk/f5tovtk_element uwing.f5
 cd ../
 python make_exploded.py --exploded 2
 mv capsStruct1_0/Scratch/tacs/tacs_output_file000.f5 CF-view/intstruct.f5
 cd CF-view/
-~/git/tacs/extern/f52vtk/f52vtk_element 52vtk_element intstruct.f5
+~/git/tacs/extern/f5tovtk/f5tovtk_element intstruct.f5
 cd ../
 python make_exploded.py --exploded 3
 mv capsStruct1_0/Scratch/tacs/tacs_output_file000.f5 CF-view/lwing.f5
 cd CF-view/
-~/git/tacs/extern/f52vtk/f52vtk_element 52vtk_element lwing.f5
+~/git/tacs/extern/f5tovtk/f5tovtk_element lwing.f5
 cd ../
 
