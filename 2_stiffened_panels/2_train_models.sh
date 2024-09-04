a8fd09de5cd4c40cb0674c6627dfbc607debaf98
seanfireball1 <sean.engelstad1@gmail.com>
2024-09-04 07:33:02 -0400

    update 2_train_models.sh again

--- 2_stiffened_panels/2_train_models.sh
+++ 2_stiffened_panels/2_train_models.sh
@@ -1,3 +1,4 @@
 # re-archive both models (useful if testing changes to kernel functions and theta)
-mpirun -n 4 python 2_train_model.py --load Nx
-mpirun -n 4 python 2_train_model.py --load Nxy
+rm *.txt
+mpirun -n 4 python 2_train_model.py --load Nx >> axial_hp_opt.txt
+mpirun -n 4 python 2_train_model.py --load Nxy >> shear_hp_opt.txt
