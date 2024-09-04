4aa156c843174116b447c9b8ee67a94b6bdc9297
seanfireball1 <sean.engelstad1@gmail.com>
2024-09-04 07:32:03 -0400

    update 2_train_models.sh

--- 2_stiffened_panels/2_train_models.sh
+++ 2_stiffened_panels/2_train_models.sh
@@ -1,3 +1,3 @@
 # re-archive both models (useful if testing changes to kernel functions and theta)
-python 2_train_model.py --load Nx
-python 2_train_model.py --load Nx
+mpirun -n 4 python 2_train_model.py --load Nx
+mpirun -n 4 python 2_train_model.py --load Nxy
