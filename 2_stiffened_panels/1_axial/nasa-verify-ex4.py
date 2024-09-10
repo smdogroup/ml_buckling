fc7c14b0468902fddec2e49975d54bd256b52692
sean-engelstad <sean.engelstad1@gmail.com>
2024-09-10 11:43:46 -0400

    nasa verification examples with their BCs

--- 2_stiffened_panels/1_axial/nasa-verify-ex4.py
+++ 2_stiffened_panels/1_axial/nasa-verify-ex4.py
@@ -1,20 +1,33 @@
 import ml_buckling as mlb
 from mpi4py import MPI
 import numpy as np
-import scipy.optimize as sopt
+
+"""
+Verification case based on:
+
+NASA TR 2215
+
+Buckling Loads of Stiffened
+Panels Subjected to Combined
+Longitudinal Compression and
+Shear:ResultsObtained
+With PASCO, EAL, and STAGS
+Computer Programs
+
+Example 4
+page 11 - overall panel dimensions
+page 15 - mesh
+page 33 - Metal blade-stiffened panel with thin skin
+
+The minimum eigenvalues for this very thin stiffAR is based on local mode though
+    and I care about verifying global modes.
+"""
 
 comm = MPI.COMM_WORLD
 
 import argparse
 # choose the aspect ratio and gamma values to evaluate on the panel
 parent_parser = argparse.ArgumentParser(add_help=False)
-parent_parser.add_argument("--rho0", type=float, default=1.0)
-parent_parser.add_argument("--gamma", type=float, default=1.0)
-parent_parser.add_argument("--stiffAR", type=float, default=1.0)
-parent_parser.add_argument("--SR", type=float, default=20.0)
-parent_parser.add_argument("--b", type=float, default=0.1)
-parent_parser.add_argument("--nelems", type=int, default=3000)
-parent_parser.add_argument("--nx_stiff_mult", type=int, default=3)
 parent_parser.add_argument('--static', default=False, action=argparse.BooleanOptionalAction)
 parent_parser.add_argument('--buckling', default=True, action=argparse.BooleanOptionalAction)
 parent_parser.add_argument('--rbe', default=True, action=argparse.BooleanOptionalAction)
@@ -22,55 +35,31 @@ parent_parser.add_argument('--lamCorr', default=False, action=argparse.BooleanOp
 
 args = parent_parser.parse_args()
 
-AR = args.rho0 # since isotropic
-stiff_AR = args.stiffAR
-# b = 0.1
-b = args.b
-a = b * AR
-# h = 5e-3
-h = b / args.SR
-nu = 0.3
-
-print(f"{a=} {b=} {h=}")
+E = 72.4e9
+nu = 0.32
 
 plate_material = mlb.CompositeMaterial(
-    E11=138e9,  # Pa
-    E22=138e9, #8.96e9
-    G12=138e9/2.0/(1+nu),
+    E11=E,  # Pa
+    E22=E, #8.96e9
+    G12=E/2.0/(1+nu),
     nu12=nu,
-    ply_angles=[0, 90, 0, 90],
+    ply_angles=[0, 90]*2,
     ply_fractions=[0.25]*4,
     ref_axis=[1, 0, 0],
 )
 
 stiff_material = plate_material
 
-# reverse solve the h_w, t_w dimensions of the stiffener
-# to produce gamma
-def gamma_resid(x):
-    _geometry = mlb.StiffenedPlateGeometry(
-        a=a, b=b, h=h, num_stiff=3, h_w=stiff_AR*x, t_w=x
-    )
-    stiff_analysis = mlb.StiffenedPlateAnalysis(
-        comm=comm,
-        geometry=_geometry,
-        stiffener_material=stiff_material,
-        plate_material=plate_material,
-    )
-    return args.gamma - stiff_analysis.gamma
-
-# approximate the h_w,t_w for gamma
-s_p = b / 4 # num_local = num_stiff + 1
-x_guess = np.power(args.gamma*s_p*h**3 / (1-nu**2), 0.25)
-xopt = sopt.fsolve(func=gamma_resid, x0=x_guess)
-# print(f"x = {xopt}")
-
-t_w = xopt[0]
-h_w = stiff_AR * t_w
 
 geometry = mlb.StiffenedPlateGeometry(
-    a=a, b=b, h=h, num_stiff=3, h_w=h_w, t_w=t_w
+    a=0.762,
+    b=0.762,
+    h=0.00127,
+    num_stiff=6, # 7 panel sections
+    h_w=0.03434,
+    t_w=0.00147,
 )
+
 stiff_analysis = mlb.StiffenedPlateAnalysis(
     comm=comm,
     geometry=geometry,
@@ -78,25 +67,17 @@ stiff_analysis = mlb.StiffenedPlateAnalysis(
     plate_material=plate_material,
 )
 
-_nelems = args.nelems
-MIN_Y = 20 / geometry.num_local
-MIN_Z = 5 #5
-N = geometry.num_local
-AR_s = geometry.a / geometry.h_w
-#print(f"AR = {AR}, AR_s = {AR_s}")
-nx = np.ceil(np.sqrt(_nelems / (1.0/AR + (N-1) / AR_s)))
-ny = max(np.ceil(nx / AR / N), MIN_Y)
-nz = max(np.ceil(nx / AR_s), MIN_Z)
 
 stiff_analysis.pre_analysis(
-    nx_plate=int(nx), #90
-    ny_plate=int(ny), #30
-    nz_stiff=int(nz), #5
-    nx_stiff_mult=args.nx_stiff_mult,
+    nx_plate=30,
+    ny_plate=14,
+    nz_stiff=2, #5
+    nx_stiff_mult=1,
     exx=stiff_analysis.affine_exx,
+    # exx = 1e-3,
     exy=0.0,
     clamped=False,
-    _make_rbe=args.rbe,  
+    _make_rbe=True,  
 )
 
 comm.Barrier()
