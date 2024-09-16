520ce3352e4b39aa08d8755c21774494a2f7a885
seanfireball1 <sean.engelstad1@gmail.com>
2024-09-15 21:18:09 -0400

    use CF to train very low rho0 values for high gamma (since no data in this regime

--- 2_stiffened_panels/1_gen_mc_axial.py
+++ 2_stiffened_panels/1_gen_mc_axial.py
@@ -16,7 +16,9 @@ comm = MPI.COMM_WORLD
 
 # argparse
 parent_parser = argparse.ArgumentParser(add_help=False)
-parent_parser.add_argument('--clear', default=False, action=argparse.BooleanOptionalAction)
+parent_parser.add_argument(
+    "--clear", default=False, action=argparse.BooleanOptionalAction
+)
 parent_parser.add_argument("--nrho0", type=int, default=50)
 parent_parser.add_argument("--nGamma", type=int, default=20)
 parent_parser.add_argument("--nelems", type=int, default=2000)
@@ -49,16 +51,15 @@ comm.Barrier()
 def get_buckling_load(rho0, gamma, solve_buckling=True, first=False):
 
     stiff_AR = 20.0
-    plate_SR = 100.0 #100.0
+    plate_SR = 100.0  # 100.0
     b = 1.0
-    h = b / plate_SR # 10 mm
+    h = b / plate_SR  # 10 mm
     nu = 0.3
     E = 138e9
     G = E / 2.0 / (1 + nu)
     # h_w = 0.08 #0.08
     # t_w = h_w / stiff_AR # 0.005
     nstiff = 1 if gamma > 0 else 0
-
 
     plate_material = mlb.CompositeMaterial(
         E11=E,  # Pa
@@ -89,12 +90,9 @@ def get_buckling_load(rho0, gamma, solve_buckling=True, first=False):
             plate_material=plate_material,
         )
 
-        return [
-            rho0-stiff_analysis.affine_aspect_ratio,
-            gamma - stiff_analysis.gamma
-        ]
+        return [rho0 - stiff_analysis.affine_aspect_ratio, gamma - stiff_analysis.gamma]
 
-    xopt = sopt.fsolve(func=gamma_rho0_resid, x0=(0.08*gamma/11.25, rho0))
+    xopt = sopt.fsolve(func=gamma_rho0_resid, x0=(0.08 * gamma / 11.25, rho0))
 
     h_w = xopt[0]
     AR = xopt[1]
@@ -102,7 +100,7 @@ def get_buckling_load(rho0, gamma, solve_buckling=True, first=False):
 
     # make a new plate geometry
     geometry = mlb.StiffenedPlateGeometry(
-        a=a, b=b, h=h, num_stiff=nstiff, h_w=h_w, t_w=h_w/stiff_AR
+        a=a, b=b, h=h, num_stiff=nstiff, h_w=h_w, t_w=h_w / stiff_AR
     )
     stiff_analysis = mlb.StiffenedPlateAnalysis(
         comm=comm,
@@ -114,12 +112,12 @@ def get_buckling_load(rho0, gamma, solve_buckling=True, first=False):
     _nelems = args.nelems
     # MIN_Y = 20 / geometry.num_local
     MIN_Y = 5
-    MIN_Z = 5 #5
+    MIN_Z = 5  # 5
     N = geometry.num_local
     AR_s = geometry.a / geometry.h_w
-    #print(f"AR = {AR}, AR_s = {AR_s}")
-    nx = np.ceil(np.sqrt(_nelems / (1.0/AR + (N-1) / AR_s)))
-    den = (1.0/AR + (N-1) * 1.0 / AR_s)
+    # print(f"AR = {AR}, AR_s = {AR_s}")
+    nx = np.ceil(np.sqrt(_nelems / (1.0 / AR + (N - 1) / AR_s)))
+    den = 1.0 / AR + (N - 1) * 1.0 / AR_s
     ny = max([np.ceil(nx / AR / N), MIN_Y])
     # nz = max([np.ceil(nx / AR_s), MIN_Z])
     nz = 3
@@ -131,16 +129,16 @@ def get_buckling_load(rho0, gamma, solve_buckling=True, first=False):
 
     if solve_buckling:
         stiff_analysis.pre_analysis(
-            nx_plate=int(nx), #90
-            ny_plate=int(ny), #30
-            nz_stiff=int(nz), #5
+            nx_plate=int(nx),  # 90
+            ny_plate=int(ny),  # 30
+            nz_stiff=int(nz),  # 5
             nx_stiff_mult=2,
             exx=stiff_analysis.affine_exx,
             exy=0.0,
             clamped=False,
-            # _make_rbe=args.rbe, 
+            # _make_rbe=args.rbe,
             _make_rbe=False,
-            _explicit_poisson_exp=True, 
+            _explicit_poisson_exp=True,
         )
 
     comm.Barrier()
@@ -152,19 +150,16 @@ def get_buckling_load(rho0, gamma, solve_buckling=True, first=False):
     if solve_buckling:
 
         tacs_eigvals, errors = stiff_analysis.run_buckling_analysis(
-            sigma=5.0, 
-            num_eig=50, #50, 100
-            write_soln=True
+            sigma=5.0, num_eig=50, write_soln=True  # 50, 100
         )
 
         # if args.static:
         # stiff_analysis.run_static_analysis(write_soln=True)
         stiff_analysis.post_analysis()
 
-
         global_lambda_star = stiff_analysis.get_mac_global_mode(
-            axial=True, 
-            min_similarity=0.7, #0.5
+            axial=True,
+            min_similarity=0.7,  # 0.5
             local_mode_tol=0.7,
         )
         # global_lambda_star = stiff_analysis.min_global_mode_eigenvalue
@@ -180,7 +175,7 @@ def get_buckling_load(rho0, gamma, solve_buckling=True, first=False):
         global_lambda_star = None
 
     # predict the actual eigenvalue
-    pred_lambda,mode_type = stiff_analysis.predict_crit_load(axial=True)
+    pred_lambda, mode_type = stiff_analysis.predict_crit_load(axial=True)
 
     # min_eigval = tacs_eigvals[0]
     # rel_err = (pred_lambda - global_lambda_star) / pred_lambda
@@ -203,7 +198,7 @@ def get_buckling_load(rho0, gamma, solve_buckling=True, first=False):
 # GENERATE DATA
 # -------------
 
-if __name__=="__main__":
+if __name__ == "__main__":
     # import argparse
     import matplotlib.pyplot as plt
     from matplotlib import cm
@@ -212,16 +207,20 @@ if __name__=="__main__":
     gamma_vec = np.geomspace(args.gammaMin, args.gammaMax, args.nGamma)
 
     ct = 0
-    for igamma,gamma in enumerate(gamma_vec):
-        for irho0,rho0 in enumerate(rho0_vec):
-            eig_CF, eig_FEA, stiff_analysis = get_buckling_load(
-                rho0=rho0, 
-                gamma=gamma
-            )
+    for igamma, gamma in enumerate(gamma_vec):
+        for irho0, rho0 in enumerate(rho0_vec):
+            eig_CF, eig_FEA, stiff_analysis = get_buckling_load(rho0=rho0, gamma=gamma)
             if comm.rank == 0:
                 print(f"{eig_CF=}, {eig_FEA=}")
-            if eig_FEA is None: 
-                eig_FEA = np.nan # just leave value as almost zero..
+
+            # for first 5 irho0 if eig_FEA doesn't exist
+            # add CF values in its place to help the ML model training in this regime
+            # will result in conservative training in this low rho0 regime
+            if irho0 < 5 and gamma >= 1.0 and eig_FEA is None:
+                eig_FEA = eig_CF
+
+            if eig_FEA is None:
+                eig_FEA = np.nan  # just leave value as almost zero..
                 continue
 
             # write out as you go so you can see the progress and if run gets killed you don't lose it all
@@ -238,7 +237,9 @@ if __name__=="__main__":
                 }
                 raw_df = pd.DataFrame(raw_data_dict)
                 first_write = ct == 1 and args.clear
-                raw_df.to_csv(raw_csv_path, mode="w" if first_write else "a", header=first_write)
+                raw_df.to_csv(
+                    raw_csv_path, mode="w" if first_write else "a", header=first_write
+                )
 
 
 # then combine the stiff and unstiff data
