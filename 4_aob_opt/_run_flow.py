659226bb3b8ee61e670600ebadb1ebc71bdba8ea
sean-engelstad <sean.engelstad1@gmail.com>
2024-09-17 10:14:32 -0400

    update aob case for comparison with Brian, NASA

---
+++ 4_aob_opt/_run_flow.py
@@ -0,0 +1,56 @@
+"""
+Run a FUN3D analysis with the Fun3dOnewayDriver
+"""
+
+from funtofem import *
+from mpi4py import MPI
+import os
+
+case = "turbulent"
+# case = "inviscid"
+
+comm = MPI.COMM_WORLD
+
+f2f_model = FUNtoFEMmodel("aob_flow")
+wing = Body.aeroelastic("wing", boundary=2)
+wing.register_to(f2f_model)
+
+# make a funtofem scenario
+cruise = Scenario.steady(
+    "cruise", 
+    steps=400, 
+    forward_coupling_frequency=60, 
+    uncoupled_steps=200
+)
+T_cruise = 220.55 #K
+q_cruise = 10.3166e3
+
+Function.lift().register_to(cruise)
+Function.drag().register_to(cruise)
+pull_up.set_temperature(T_ref=T_cruise, T_inf=T_cruise)
+pull_up.set_flow_ref_vals(qinf=q_cruise)
+pull_up.register_to(f2f_model)
+
+
+# DISCIPLINE INTERFACES AND DRIVERS
+# -----------------------------------------------------
+
+solvers = SolverManager(comm)
+solvers.flow = Fun3d14Interface(
+    comm,
+    f2f_model,
+    fun3d_dir="cfd",
+    forward_stop_tolerance=1e-13,
+    forward_min_tolerance=1e-8,
+)
+my_transfer_settings = TransferSettings(npts=200)
+fun3d_driver = OnewayAeroDriver(
+    solvers, f2f_model, transfer_settings=my_transfer_settings
+)
+fun3d_driver.solve_forward()
+
+# write an aero loads file
+aero_loads_file = os.path.join(
+    os.getcwd(), "cfd", "loads", "uncoupled_turb_loads.txt"
+)
+f2f_model.write_aero_loads(comm, aero_loads_file)
---
+++ 4_aob_opt/geometry/_mesh_fun3d_egads_mirror.py
@@ -0,0 +1,120 @@
+from pathlib import Path
+from funtofem.interface import Fun3dModel, Fun3dBC
+from mpi4py import MPI
+
+here = Path(__file__).parent
+comm = MPI.COMM_WORLD
+
+# Set whether to build an inviscid or viscous mesh
+# ------------------------------------------------
+# case = "inviscid"
+case = "turbulent"
+
+project_name = "gbm"
+# ------------------------------------------------
+
+# Set up FUN3D model, AIMs, and turn on the flow view
+# ------------------------------------------------
+fun3d_model = Fun3dModel.build(
+    csm_file="gbm.csm",
+    comm=comm,
+    project_name=project_name,
+    problem_name="capsFluidEgads",
+    volume_mesh="aflr3",
+    surface_mesh="egads",
+    verbosity=0,
+)
+mesh_aim = fun3d_model.mesh_aim
+fun3d_aim = fun3d_model.fun3d_aim
+fun3d_aim.set_config_parameter("view:flow", 1)
+fun3d_aim.set_config_parameter("view:struct", 0)
+fun3d_aim.set_config_parameter("symPlane", 0)
+fun3d_aim.set_config_parameter("mirror", 1)
+
+# if comm.rank == 0:
+#    aflr3_aim = mesh_aim.volume_aim.aim
+#    aflr3_aim.input.Mesh_Format = "AFLR3"
+
+# ------------------------------------------------
+
+refinement = 2
+
+mesh_aim.surface_aim.set_surface_mesh(
+    edge_pt_min=3,
+    edge_pt_max=200,
+    mesh_elements="Mixed",
+    global_mesh_size=0,
+    max_surf_offset=0.0008 if refinement == 1 else 0.0016,
+    max_dihedral_angle=15,
+)
+
+
+# finer refinement
+# num_pts_up = 60
+# num_pts_bot = 60
+# num_pts_y = 70
+# num_finite_te = 4
+# horiz_spacing = [0.08, 0.05]
+
+# coarser refinement
+num_pts_up = 40 #60
+num_pts_bot = 40 #60
+num_pts_y = 70 #70
+num_finite_te = 4
+
+horiz_spacing = [0.16, 0.1]
+
+mesh_aim.surface_aim.aim.input.Mesh_Sizing = {
+    "teHorizEdgeMesh": {
+        "numEdgePoints": num_pts_y,
+        "edgeDistribution": "Tanh",
+        "initialNodeSpacing": horiz_spacing,
+    },
+    "teVertEdgeMesh": {
+        "numEdgePoints": num_finite_te,
+    },
+    "farfieldEdgeMesh": {
+        "numEdgePoints": 8,
+    },
+    "leEdgeMesh": {
+        "numEdgePoints": num_pts_y,
+        "edgeDistribution": "Tanh",
+        "initialNodeSpacing": horiz_spacing,
+    },
+    "tipUpperEdgeMesh": {
+        "numEdgePoints": num_pts_up,
+        "edgeDistribution": "Tanh",
+        "initialNodeSpacing": [0.01, 0.004],
+    },
+    "tipLowerEdgeMesh": {
+        "numEdgePoints": num_pts_bot,
+        "edgeDistribution": "Tanh",
+        "initialNodeSpacing": [0.004, 0.01],
+    },
+    # no root in mirrored mesher
+    # "rootUpperEdgeMesh": {
+    #     "numEdgePoints": num_pts_up,
+    #     "edgeDistribution": "Tanh",
+    #     "initialNodeSpacing": [0.005, 0.002],
+    # },
+    # "rootLowerEdgeMesh": {
+    #     "numEdgePoints": num_pts_bot,
+    #     "edgeDistribution": "Tanh",
+    #     "initialNodeSpacing": [0.002, 0.005],
+    # },
+    # "rootMesh" : {"tessParams": [0.05, 0.01, 20.0]},
+    "tipMesh": {"tessParams": [0.05, 0.01, 20.0]},
+}
+
+Fun3dBC.Farfield(caps_group="farfield").register_to(fun3d_model)
+
+if case == "inviscid":
+    Fun3dBC.inviscid(caps_group="wing").register_to(fun3d_model)
+else:
+    mesh_aim.volume_aim.set_boundary_layer(
+        initial_spacing=1e-6, max_layers=60, thickness=0.25, use_quads=True
+    )
+    Fun3dBC.viscous(caps_group="wing", wall_spacing=0.1).register_to(fun3d_model)
+
+fun3d_model.setup()
+fun3d_aim.pre_analysis()
