c91fa98c4153360a068648480191c7014eafc166
Sean Engelstad <sean.engelstad1@gmail.com>
2024-10-16 13:22:29 -0400

    update figures 10-11 plots

--- 2_stiffened_panels/figures10-11/gamma/plot_v3.py
+++ 2_stiffened_panels/figures10-11/gamma/plot_v3.py
@@ -14,40 +14,41 @@ assert args.load in ["Nx", "Nxy"]
 
 # reload the data
 ibin = 0 #1
-data = np.load(f"{args.load}-xi-data.npz")
+data = np.load(f"{args.load}-gamma-data.npz")
 X = data["X"]; Y = data["Y"]
-XI, AR, KMIN = data["XI"], data["AR"], data["KMIN"]
+GAMMA, AR, KMIN = data["GAMMA"], data["AR"], data["KMIN"]
 
 # bins for the data (in log space)
-# xi_bins = [[0.2, 0.4], [0.4, 0.6], [0.6, 0.8], [0.8, 1.05]]
+xi_bins = [[0.2, 0.4], [0.4, 0.6], [0.6, 0.8], [0.8, 1.05]]
 rho0_bins = [[-2.5, -1.0], [-1.0, 0.0], [0.0, 1.0], [1.0, 2.5]]
 zeta_bins = [[0.0, 0.1], [0.1, 0.5], [0.5, 1.0], [1.0, 2.5]]
 # gamma_bins = [[0.0, 0.1], [0.1, 1.0], [1.0, 3.0], [3.0, 5.0]]
-gamma_bins = [[0.0, 0.1], [0.1, 1.0], [1.0, 2.0], [2.0, 3.0]]
-
-xi_vec = np.geomspace(1+0.25, 1+1.05, 6+1)-1
-xi_bins = [[xi_vec[i], xi_vec[i+1]] for i in range(6)]
+# gamma_bins = [[0.0, 0.1], [0.1, 1.0], [1.0, 2.0], [2.0, 3.0]]
+gamma_vec = np.geomspace(1+0.0, 1+3.0, 6+1)-1
+gamma_bins = [[gamma_vec[i], gamma_vec[i+1]] for i in range(6)]
 
 # 3d plot of rho_0, gamma, lam_star for a particular xi and zeta range
-gamma_bin = [0.0, 0.1]
-gamma_mask = np.logical_and(gamma_bin[0] <= X[:, 3], X[:, 3] <= gamma_bin[1])
-avg_gamma = 0.5 * (gamma_bin[0] + gamma_bin[1])
+xi_bin = [0.5, 0.7]
+xi_mask = np.logical_and(xi_bin[0] <= X[:, 0], X[:, 0] <= xi_bin[1])
+avg_xi = 0.5 * (xi_bin[0] + xi_bin[1])
 
-zeta_bin = [0.0, 1.0]
+zeta_bin = [0.2, 0.4]
 zeta_mask = np.logical_and(zeta_bin[0] <= X[:, 2], X[:, 2] <= zeta_bin[1])
 avg_zeta = 0.5 * (zeta_bin[0] + zeta_bin[1])
-gamma_zeta_mask = np.logical_and(gamma_mask, zeta_mask)
+xi_zeta_mask = np.logical_and(xi_mask, zeta_mask)
 
-plt.figure(f"3d rho_0, xi, lam_star")
+plt.figure(f"3d rho_0, gamma, lam_star")
 ax = plt.axes(projection="3d", computed_zorder=False)
 
+# colors = plt.cm.jet(np.linspace(0.0, 1.0, len(gamma_bins)))
 colors = mlb.six_colors2[::-1]
 
-for ixi, xi_bin in enumerate(xi_bins):
+for igamma, gamma_bin in enumerate(gamma_bins):
 
-    xi_mask = np.logical_and(xi_bin[0] <= X[:, 0], X[:, 0] <= xi_bin[1])
-    avg_xi = 0.5 * (xi_bin[0] + xi_bin[1])
-    mask = np.logical_and(gamma_zeta_mask, xi_mask)
+    gamma_mask = np.logical_and(
+        gamma_bin[0] <= X[:, 3], X[:, 3] <= gamma_bin[1]
+    )
+    mask = np.logical_and(xi_zeta_mask, gamma_mask)
 
     rho0_bin = [np.log(0.3), np.log(10.0)]
     rho0_mask = np.logical_and(
@@ -59,25 +60,25 @@ for ixi, xi_bin in enumerate(xi_bins):
     Y_in_range = Y[mask, :]
 
     ax.scatter(
-        X_in_range[:, 0],
+        X_in_range[:, 3],
         X_in_range[:, 1],
         Y_in_range[:, 0],
         s=20,
-        color=colors[ixi],
+        color=colors[igamma],
         edgecolors="black",
-        zorder=2 + ixi,
+        zorder=2 + igamma,
     )
 
 # plot the model curve
 # Creating plot
 face_colors = cm.Grays(0.4 * KMIN / KMIN)
 ax.plot_surface(
-    XI,
+    GAMMA,
     AR,
     KMIN,
     antialiased=True,
     facecolors=face_colors,
-    alpha=0.5,
+    alpha=0.6,
     # edgecolor='none',
     linewidth=0.3,
     # edgecolor='lightgrey',
@@ -87,7 +88,7 @@ ax.plot_surface(
 
 # save the figure
 fs1 = 16
-ax.set_xlabel(r"$\mathbf{\ln(1+\xi)}$", fontsize=fs1, fontweight='bold')
+ax.set_xlabel(r"$\mathbf{\ln(1+\gamma)}$", fontsize=fs1, fontweight='bold')
 ax.set_ylabel(r"$\mathbf{\ln(\rho_0)}$", fontsize=fs1, fontweight='bold')
 if args.load == "Nx":
     ax.set_zlabel(r"$\mathbf{\ln(N_{11,cr}^*)}$", fontsize=fs1, fontweight='bold')
@@ -104,5 +105,5 @@ ax.yaxis.set_tick_params(labelsize=14)
 ax.zaxis.set_tick_params(labelsize=14)
 # plt.title(f"")
 # plt.show()
-plt.savefig(f"{args.load}-v2.png", dpi=400)
-plt.close(f"3d rho_0, xi, lam_star")
+plt.savefig(f"{args.load}-v3.png", dpi=400)
+plt.close(f"3d rho_0, gamma, lam_star")
