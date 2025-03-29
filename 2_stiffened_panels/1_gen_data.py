fix MPI broadcast issues

2_stiffened_panels/1_gen_data.py
@@ -61,6 +61,7 @@ if __name__ == "__main__":
     # only generates stiffened data, unstiffened we can downsample but we already have that data
     
     logrho_vec = np.linspace(-2.0, 2.0, 20) #ln(rho0 or rho0^*)
+    #logrho_vec = np.linspace(-2.0, 2.0, 3) # debugging
     loggamma_vec = np.linspace(0.1, 3.0, 10) # ln(1+gamma)
     # loggamma_vec = np.linspace(0.01, 3.0, 10) # ln(1+gamma)
 
@@ -133,6 +134,10 @@ if __name__ == "__main__":
                 else:
                     rho0 = np.exp(log_rho)
 
+                if comm.rank == 0: 
+                    print("\n----------------------------------------------------\n")
+                    print(f"{num_models=} : new design with {gamma=} {rho0=} {num_stiff=}\n\n", flush=True)
+
                 # choose the number of stiffeners based on (log_rho, log_gamma)
                 # ------------------------------------------------------------
 
@@ -165,14 +170,19 @@ if __name__ == "__main__":
                     b=1.0,
                     stiff_AR=15.0 # works better than 20.0 for getting more high gamma, low rho0 data
                 )
+                comm.Barrier()
 
                 # loop to check stiffener counts
-                stiffened_region = log_gamma < 0.6 * (log_rho + 1.5)
+                stiffened_region = log_gamma >= 0.6 * (log_rho + 1.5)
 
                 if stiffened_region: # check if we need to increase # stiffeners
-                    print("In stiffened region, check if we need more stiffeners:\n\n")
-                    while (num_stiff < 7):
-                        print(f"\tcheck increasing num_stiff to {num_stiff+1}")
+                    if comm.rank == 0: print("In stiffened region, check if we need more stiffeners:\n\n")
+                    eig_FEA = None
+                    if num_stiff == 9: # the max
+                        eig_CF, eig_FEA, stiff_analysis, new_eig_dict, dt = orig_output
+
+                    while (num_stiff < 9):
+                        if comm.rank == 0: print(f"\tcheck increasing num_stiff to {num_stiff+1}")
                         # propose increasing the stiffener count
                         new_output = get_buckling_load(
                             comm,
@@ -189,22 +199,27 @@ if __name__ == "__main__":
                             b=1.0,
                             stiff_AR=15.0 # works better than 20.0 for getting more high gamma, low rho0 data
                         )
+                        comm.Barrier()
 
                         # compare FEA eigenvalues aka entry 1
                         if orig_output[1] is None and new_output[1] is not None:
                             num_stiff += 1 # increase stiffener count
                             eig_CF, eig_FEA, stiff_analysis, new_eig_dict, dt = new_output
-                            break
+                            orig_output = new_output # so we compare to new working eigval
+                            # continue, but still need to check it's converged too
                         elif orig_output[1] is not None and new_output[1] is None:
                             eig_CF, eig_FEA, stiff_analysis, new_eig_dict, dt = orig_output
+                            # orig_output stays the same
                             break
                         elif orig_output[1] is None and new_output[1] is None:
+                            # both None, increase # stiffeners and try again
                             eig_CF, eig_FEA, stiff_analysis, new_eig_dict, dt = orig_output
-                            break # None, we going to be skipping this trial
+                            num_stiff += 1 # both None, no need to change orig_output
                         else: # both not None
                             rel_diff = abs( (orig_output[1] - new_output[1]) / orig_output[1] )
                             if rel_diff < 0.1: # don't chang enumber of stiffeners
-                                eig_CF, eig_FEA, stiff_analysis, new_eig_dict, dt = orig_output
+                                # might as well use new_output since we ran it
+                                eig_CF, eig_FEA, stiff_analysis, new_eig_dict, dt = new_output
                                 break
                             else: # makes a difference, increase stiffener count
                                 num_stiff += 1 # increase stiffener count
@@ -224,7 +239,8 @@ if __name__ == "__main__":
                 # -------------------------                
 
                 if comm.rank == 0:
-                    print(f"{eig_CF=}, {eig_FEA=} {stiff_analysis.gamma=}")
+                    print(f"model {num_models} : {eig_CF=}, {eig_FEA=} {stiff_analysis.gamma=}", flush=True)
+                    print("---------------------------------\n\n")
 
                 if eig_FEA is None:
                     eig_FEA = np.nan  # just leave value as almost zero..

2_stiffened_panels/src/ant_optimization.py
@@ -22,6 +22,8 @@ def ant_optimization(
     plot_debug:bool=False,
     hs_ind:int=0,
 ):
+    
+    print(f"ant opt: {comm.rank} checkpt0", flush=True)
      
     target_log_gamma = np.log10(1.0+gamma)
     # target_log_rho0 = np.log10(rho0)
@@ -63,6 +65,8 @@ def ant_optimization(
                 min_err = resid_norm
     if comm.rank == 0: print(f"{guess_x0=} {min_err=}")
 
+    print(f"ant opt: {comm.rank} checkpt1", flush=True)
+
     # now solve in 10x10 different spots (to try and find the multiple unique solutions)
     log_hw_vec = np.linspace(-3, 2.0, 10); dlog_hw = np.diff(log_hw_vec)[0]
     log_rho_vec = np.linspace(-3, 3.0, 10); dlog_rho = np.diff(log_rho_vec)[0]
@@ -85,8 +89,9 @@ def ant_optimization(
         if len(solns) > 0: break
                 
 
+    print(f"ant opt: {comm.rank} checkpt2", flush=True)
     if comm.rank == 0:
-        print(f"ant finding method: {solns}")
+        print(f"ant finding method: {solns}", flush=True)
 
     if plot_debug:
         import matplotlib.pyplot as plt
@@ -116,7 +121,9 @@ def ant_optimization(
     AR = 10**(xopt[1])
     a = b * AR
 
+    print(f"ant opt: {comm.rank} checkpt3", flush=True)
+
     if comm.rank == 0:
-        print(f"soln of R(xhat): {h_w=} {AR=}")
+        print(f"soln of R(xhat): {h_w=} {AR=}", flush=True)
 
     return h_w, AR, a

2_stiffened_panels/src/buckling_analysis.py
@@ -51,6 +51,8 @@ def get_buckling_load(
         plot_debug=plot_debug,
         hs_ind=ant_hs_ind,
     )
+
+    print(f"{comm.rank} checkpt-2", flush=True)
     
 
     # make a new plate geometry
@@ -66,8 +68,10 @@ def get_buckling_load(
         plate_material=plate_material,
     )
 
+    print(f"{comm.rank} checkpt-1", flush=True)
+
     #if comm.rank == 0:
-    print(f"achieved {stiff_analysis.gamma=} {stiff_analysis.affine_aspect_ratio=}")
+    print(f"achieved {stiff_analysis.gamma=} {stiff_analysis.affine_aspect_ratio=}", flush=True)
     # exit()
 
     _nelems = nelems
@@ -81,6 +85,7 @@ def get_buckling_load(
     nz = 3
 
     # print(f"{nx=} {ny=} {nz=}")
+    print(f"{comm.rank} checkpt0", flush=True)
 
     if solve_buckling:
         stiff_analysis.pre_analysis(
@@ -95,6 +100,7 @@ def get_buckling_load(
             _explicit_poisson_exp=True,
         )
 
+    print(f"{comm.rank} checkpt1", flush=True)
     comm.Barrier()
 
     if debug:
@@ -106,7 +112,9 @@ def get_buckling_load(
         tacs_eigvals, errors = stiff_analysis.run_buckling_analysis(
             sigma=sigma_eig, num_eig=100, write_soln=True  # 50, 100
         )
+        print(f"{comm.rank} checkpt2", flush=True)
         stiff_analysis.post_analysis()
+        print(f"{comm.rank} checkpt3", flush=True)
 
         eig_FEA = None
         rho0 = stiff_analysis.affine_aspect_ratio
@@ -135,15 +143,22 @@ def get_buckling_load(
                         print(f"mode {imode} was tracked but it is a local mode..")
             
         else: # just use heuristic
-            eig_FEA = stiff_analysis.get_mac_global_mode(
-                axial=is_axial,
-                min_similarity=0.75,  # 0.5
-                local_mode_tol=0.75, #0.8
-            )
+            if comm.rank == 0:
+                eig_FEA = stiff_analysis.get_mac_global_mode(
+                    axial=is_axial,
+                    min_similarity=0.75,  # 0.5
+                    local_mode_tol=0.75, #0.8
+                )
 
-        if eig_FEA is not None:
+        comm.Barrier()
+
+        if eig_FEA is not None and comm.rank == 0:
             if np.abs(errors[stiff_analysis.min_global_mode_index]) > 1e-8:
                 eig_FEA = None
+
+        print(f"{comm.rank=} pre-broadcast")
+        eig_FEA = comm.bcast(eig_FEA, root=0)
+        print(f"{comm.rank=} {eig_FEA=}")
 
         if comm.rank == 0:
             stiff_analysis.print_mode_classification()